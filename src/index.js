b099cffb257bed2a1e0bd4b820d1ebe272ab758a
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-05-04 19:57:34 -0400

    Add tab switching feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { createHomeTab } from "./home-tab";
 import { createMenuTab } from "./menu-tab";
+import { createAboutTab } from "./about-tab";
 
 const rootElement = document.querySelector("#content");
+const navigation = document.querySelector('nav');
 const loadHomeTab = createHomeTab(document, rootElement);
+navigation.querySelector('#nav-home').addEventListener('click', (event) => loadHomeTab());
 const loadMenuTab = createMenuTab(document, rootElement);
+navigation.querySelector('#nav-menu').addEventListener('click', (event) => loadMenuTab());
+const loadAboutTab = createAboutTab(document, rootElement);
+navigation.querySelector('#nav-about').addEventListener('click', (event) => loadAboutTab());
+
 loadHomeTab();
-loadMenuTab();
